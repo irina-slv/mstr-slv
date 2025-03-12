7f5860532244230b2d842f5a6129a11b7da94df9
irina-slv <rakotovaoirina@gmail.com>
2025-03-12 10:08:01 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,6 +1,29 @@
 <?php
-    define ('SERVEUR_BD','localhost');
-	define ('LOGIN_BD','root');
-	define ('PASS_BD','BastianIrina');
-    define ('NOM_BD','Vitrine_Voyage');
+    $db_host ='localhost';
+    $db_user ='root';
+    $db_password = 'root';
+    $db_db = 'user';
+
+    $mysqli = @new mysqli(
+        $db_host,
+        $db_user,
+        $db_password,
+        $db_db
+    );
+    
+    if ($mysqli->connect_error){
+        echo 'Errno:'.$mysqli->connect_errno ;
+        echo '<br>';
+        echo 'Error :' .$mysqli->connect_error;
+        exit();
+    }
+
+    $var = 'Vous êtes connéctes';
+    echo 'Vous êtes connéctes';
+    echo '<br>';
+    echo 'Host information : ' .$mysqli->host_info;
+    echo '<br>';
+    echo 'Protocol version :' .$mysqli->protocol_version ;
+
+    $mysqli->close();
 ?>
